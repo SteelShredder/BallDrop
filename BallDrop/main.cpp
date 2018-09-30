3875bacdda44b499908a1db091f2c9675474d64a
Gavin Gavington <GavinGavington@gavins-macbook-air.local>
2018-09-30 10:25:01 -0700

    Added program.

--- BallDrop/main.cpp
+++ BallDrop/main.cpp
@@ -5,10 +5,57 @@
 //  Created by Gavin Gavington on 9/29/18.
 //
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+/*
+ Program flow
+ inputHeight in metres
+ velocity = 0
+ Gravity in myConstant namespace
+ Output distance fallen for 0,1,2,3,4,5
+ Distance fallen = gravity * seconds * seconds / 2
+ 
+ */
+
+#include <iostream>
+#include "myConstants.h"
+double distanceFallen(int tSeconds)
+{
+    return constants::gravity * tSeconds * tSeconds / 2;
+}
+int main()
+{
+    std::cout << "Input height in meters" << std::endl;
+    double height;
+    std::cin >> height;
+    if (height-distanceFallen(0) > 0)
+    {
+        std::cout << distanceFallen(0) << std::endl;;
+    }
+    height -= distanceFallen(0);
+    if (height-distanceFallen(1) > 0)
+    {
+        std::cout << distanceFallen(1) << std::endl;;
+    }
+    height -= distanceFallen(1);
+    if (height-distanceFallen(2) > 0)
+    {
+        std::cout << distanceFallen(2) << std::endl;;
+    }
+    height -= distanceFallen(2);
+    if (height-distanceFallen(3) > 0)
+    {
+        std::cout << distanceFallen(3) << std::endl;;
+    }
+    height -= distanceFallen(3);
+    if (height-distanceFallen(4) > 0)
+    {
+        std::cout << distanceFallen(4) << std::endl;;
+    }
+    height -= distanceFallen(4);
+    if (height-distanceFallen(5) > 0)
+    {
+        std::cout << distanceFallen(5) << std::endl;;
+    }
+    height -= distanceFallen(5);
     return 0;
 }
---
+++ BallDrop/myConstants.h
@@ -0,0 +1,14 @@
+//
+//  myConstants.h
+//  BallDrop
+//
+//  Created by Gavin Gavington on 9/29/18.
+//
+
+#ifndef myConstants_h
+#define myConstants_h
+namespace constants
+{
+    constexpr double gravity(9.8);
+}
+#endif /* myConstants_h */
